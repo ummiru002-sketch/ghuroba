c4bd72b463a6f9114f38b28b15e7d9090ef7a89c
ummiru002-sketch <ummiru002@gmail.com>
2025-12-07 11:25:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
 db.init_app(app)
 with app.app_context():
-    db.drop_all()
-    db.create_all()
+   
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
